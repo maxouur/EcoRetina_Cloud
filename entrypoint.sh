0e55dff1bdcb1914f20cdf41ee6a9ef6ce659b6c
maxouur <136832133+maxouur@users.noreply.github.com>
2026-07-03 16:14:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-# 1. Écran virtuel et gestionnaire de fenêtres
-Xvfb :1 -screen 0 1280x800x24 &
-sleep 2
+# En cas d'erreur, on continue
+set -e
+
+# 1. Configurer et forcer l'affichage sur le Display :0
+export DISPLAY=:0
+
+# Supprimer d'éventuels verrous d'une session précédente
+rm -f /tmp/.X0-lock
+
+# 2. Lancer l'écran virtuel (Xvfb) sur le display :0
+Xvfb :0 -screen 0 1280x800x24 &
+sleep 3
+
+# 3. Lancer le gestionnaire de fenêtres Fluxbox
 fluxbox &
-sleep 1
+sleep 2
 
-# 2. Serveur VNC graphique (Port 5900)
-x11vnc -display :1 -nopw -forever -shared -listen localhost &
-sleep 1
+# 4. Lancer le serveur graphique x11vnc sur le display :0
+x11vnc -display :0 -nopw -forever -shared &
+sleep 2
 
-# 3. Lancement du pont Web noVNC officiel et mis à jour
-# On utilise la commande standard 'novnc_proxy' qui remplace 'launch.sh'
-novnc_proxy --vnc localhost:5900 --listen $PORT
+# 5. Lancer ton vrai logiciel CustomTkinter en arrière-plan
+python GUI_EcoRetina_AIagent.py &
+sleep 2
+
+# 6. Lancer le proxy de noVNC via son chemin Linux absolu
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5900 --listen $PORT
