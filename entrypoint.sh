8b4b78f79666f720a4f6807f108a9923d64e781b
maxouur <136832133+maxouur@users.noreply.github.com>
2026-07-03 15:53:50 +0200

    Enhance entrypoint.sh with detailed comments and options
    Updated entrypoint.sh to specify resolution and web directory for noVNC.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# 1. Lancer l'écran virtuel Linux en arrière-plan
+# 1. Lancer l'écran virtuel Linux en arrière-plan (Résolution 1280x800)
 Xvfb :1 -screen 0 1280x800x24 &
 sleep 2
 
-# 2. Lancer le gestionnaire de fenêtres
+# 2. Lancer le gestionnaire de fenêtres Fluxbox
 fluxbox &
 sleep 1
 
@@ -12,8 +12,8 @@ sleep 1
 x11vnc -display :1 -nopw -forever -shared -listen localhost &
 sleep 1
 
-# 4. Lancer le pont Web (noVNC) sur le port que Render exige
-/usr/share/novnc/utils/launch.sh --vnc localhost:5900 --listen $PORT &
+# 4. Lancer le pont Web (noVNC) sur le port choisi par Render avec le dossier web
+/usr/share/novnc/utils/launch.sh --vnc localhost:5900 --listen $PORT --web /usr/share/novnc &
 sleep 2
 
 # 5. Lancer ton vrai logiciel CustomTkinter !
