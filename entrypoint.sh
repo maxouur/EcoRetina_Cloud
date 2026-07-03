48bf0ba5081fb590d71dfa9dd4b869d4571622a5
maxouur <136832133+maxouur@users.noreply.github.com>
2026-07-03 15:57:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# 1. Lancer l'écran virtuel Linux en arrière-plan (Résolution 1280x800)
+# 1. Écran virtuel et gestionnaire de fenêtres
 Xvfb :1 -screen 0 1280x800x24 &
 sleep 2
-
-# 2. Lancer le gestionnaire de fenêtres Fluxbox
 fluxbox &
 sleep 1
 
-# 3. Lancer le serveur VNC graphique
+# 2. Serveur VNC graphique (Port 5900)
 x11vnc -display :1 -nopw -forever -shared -listen localhost &
 sleep 1
 
-# 4. Lancer le pont Web (noVNC) sur le port choisi par Render avec le dossier web
-/usr/share/novnc/utils/launch.sh --vnc localhost:5900 --listen $PORT --web /usr/share/novnc &
+# 3. noVNC (On le décale sur le port 6080 en interne)
+/usr/share/novnc/utils/launch.sh --vnc localhost:5900 --listen 6080 &
 sleep 2
 
-# 5. Lancer ton vrai logiciel CustomTkinter !
-python GUI_EcoRetina_IAagent.py
+# 4. Lancer ton vrai logiciel CustomTkinter !
+python GUI_EcoRetina_IAagent.py &
+
+# 5. Le "Bouclier" Render : Un serveur Python sur le port officiel exigé par Render
+# Il va servir le dossier noVNC et répondre directement au robot de Render
+cd /usr/share/novnc
+python3 -m http.server $PORT
